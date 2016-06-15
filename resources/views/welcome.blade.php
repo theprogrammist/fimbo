13bd889f20ac4834cf9658171ea43d336cdadb35
theprogrammist <polyanskiy@mail.ru>
2016-06-15 08:17:31 +0300

    link fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
             sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed
             quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam
             quaerat voluptatem.</p>
-        <div class="col12"><a href="javascript:void(0);" class="btn">поехали</a>
+        <div class="col12"><a href="{{ url('/register') }}" class="btn">поехали</a>
         </div>
     </div>
     </div>
@@ -131,7 +131,7 @@
         </div>
     </div>
     <div class="screen5">
-        <h2 class="h2">Все понятно?</h2><a href="javascript:void(0);" class="btn btn_question">начинаем!</a>
+        <h2 class="h2">Все понятно?</h2><a href="{{ url('/register') }}" class="btn btn_question">начинаем!</a>
     </div>
 <div class="popup js-popup js-popup-video">
     <a href="javascript:void(0);" class="popup__close js-popup-close"></a>
