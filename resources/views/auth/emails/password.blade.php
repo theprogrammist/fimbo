c6f4f81dd1ee51684b661364d84c631ac68d30d2
theprogrammist <polyanskiy@mail.ru>
2016-06-15 08:49:25 +0300

    some rusifications

--- resources/views/auth/emails/password.blade.php
+++ resources/views/auth/emails/password.blade.php
@@ -1,1 +1,1 @@
-Click here to reset your password: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
+Для восстановления пароля перейдите по этой ссылке: <a href="{{ $link = url('password/reset', $token).'?email='.urlencode($user->getEmailForPasswordReset()) }}"> {{ $link }} </a>
