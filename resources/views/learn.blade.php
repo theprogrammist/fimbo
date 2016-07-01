6dd3577f401ab40a5d673ed450896332f3c9e947
theprogrammist <polyanskiy@mail.ru>
2016-07-01 17:32:27 +0300

    rand-style-in-courses-blocks-replased-by-cycle

--- resources/views/learn.blade.php
+++ resources/views/learn.blade.php
@@ -9,10 +9,12 @@
     <div class="content-full content-full_nopad">
         <div class="decided">
             <h2 class="h2 decided__h2 decided__h2_nobg">Учись</h2>
+            <?php $i = 0;?>
             @foreach(App\Page::whereType('lection')->whereParentId(null)->distinct()->get(['course']) as $course)
-            <div class="dropdown-tab js-droptab dropdown-tab_{{ Array('econom','mat','erud')[rand(0,2)] }}"><a href="javascript:void(0);" class="dropdown-tab__link js-droptab-link"> {{ $course->course }}</a>
+            <?php $i++;?>
+            <div class="dropdown-tab js-droptab dropdown-tab_{{ Array('econom','mat','erud')[$i] }}"><a href="javascript:void(0);" class="dropdown-tab__link js-droptab-link"> {{ $course->course }}</a>
                 <div class="dropdown-tab__content">
-                    <div class="slider-five slider-five_learn slider-five_learn_{{ Array('econom','mat','erud')[rand(0,2)] }}">
+                    <div class="slider-five slider-five_learn slider-five_learn_{{ Array('econom','mat','erud')[$i] }}">
                         <div class="js-slider-five">
 
                             @include('block.lection-slider',['courseName' => $course->course])
