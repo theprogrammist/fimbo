de0c2bda8a1b199a4ff599df68cdea1fb254bae3
theprogrammist <polyanskiy@mail.ru>
2016-07-25 14:50:08 +0300

    problem_slider_shows_user_solution_attempts

--- resources/views/solve.blade.php
+++ resources/views/solve.blade.php
@@ -8,31 +8,57 @@
             <div class="js-slider-five">
 
 
-                @foreach(\App\Problem::all() as $lct)
+                @foreach(\App\Problem::all() as $prblm)
                     <div class="slider__item">
                         <div class="slider__item-block">
                             <div class="slider__item-image">
                                 <img src="/img/slider-decide-img3.jpg" alt="slider-img" class="slider__item-img">
                             </div>
 
-                            <div class="{{ $lct->course->color }}Option slider__item-category">{{ $lct->course->title }}</div>
-                            <div class="slider__item-progress slider__item-progress_yes"></div>
+                            <div class="{{ $prblm->course->color }}Option slider__item-category">{{ $prblm->course->title }}</div>
+                            <div class="slider__item-progress slider__item-progress_{{
 
-                            <a href="{{ url('problem/') . '/' . $lct->id . '/'  }}" class="slider-five_learn_olive">
+                                (
+                                    (Auth::user()->problems->find($prblm->id))
+                                    &&
+                                    !(Auth::user()->problems->find($prblm->id)->pivot->success)
+                                ) ? 'no' : 'yes'
 
-                                <div class="slider__item-title slider__item-title_{{ $lct->course->color }}">
-                                    <div class="slider__item-span">{{ $lct->title }}</div>
+                            }}"></div>
+
+                            <a href="{{ url('problem/') . '/' . $prblm->id . '/'  }}" class="slider-five_learn_olive">
+
+                                <div class="slider__item-title slider__item-title_{{ $prblm->course->color }}">
+                                    <div class="slider__item-span">{{ $prblm->title }}</div>
                                 </div>
                             </a>
 
-                            <div class="slider__item-point"><span class="slider__item-point-span">{{ $lct->score }} </span>очков</div>
+                            <div class="slider__item-point"><span class="slider__item-point-span">{{ $prblm->score }} </span>очков</div>
 
-                            <div class="slider__item-level {{ (empty($lct->difficulty))?App\Page::$difficultyColors[1] : App\Page::$difficultyColors[$lct->difficulty] }}Option">
-                                {{ (empty($lct->difficulty))?App\Page::$difficultyNames[1] : App\Page::$difficultyNames[$lct->difficulty] }}</div>
+                            <div class="slider__item-level {{ (empty($prblm->difficulty))?App\Page::$difficultyColors[1] : App\Page::$difficultyColors[$prblm->difficulty] }}Option">
+                                {{ (empty($prblm->difficulty))?App\Page::$difficultyNames[1] : App\Page::$difficultyNames[$prblm->difficulty] }}</div>
+
+                            @if(Auth::user()->problems->find($prblm->id) && !(Auth::user()->problems->find($prblm->id)->pivot->success))
+                                <div class="slider__item-blocked">
+                                    <div class="slider__item-blocked-text">Эта задача была {{
+
+                                        (Auth::user()->problems->find($prblm->id)->pivot->attempt == 2) ? 'дважды' : ''
+
+                                    }} решена неверно.
+                                        <br>Решать её ещё раз нельзя.
+                                        <br>Переходи к следующей.</div>
+                                </div>
+                            @elseif(Auth::user()->problems->find($prblm->id)) && (Auth::user()->problems->find($prblm->id)->pivot->success))
+                            <div class="slider__item-blocked">
+                                <div class="slider__item-blocked-text">Эта задача успешно решена.</div>
+                            </div>
+                            @endif
+
+
                         </div>
                         <div class="slider__item-text">
                             <div class="slider__item-text-hidden">
-                                {!! implode(' ', array_slice(explode(' ', strip_tags($lct->description)), 0, 16)) !!} ...
+                                {!! implode(' ', array_slice(explode(' ', strip_tags($prblm->description)), 0, 16)) !!} ...
                             </div>
                         </div>
                     </div>
